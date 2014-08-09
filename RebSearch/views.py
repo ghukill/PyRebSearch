c15b8062e93e50bf5dd57ecff62acc9a659f2e43
root <root@mitten.(none)>
2014-08-09 14:53:16 +0000

    Zotero indexing

--- RebSearch/views.py
+++ RebSearch/views.py
@@ -60,29 +60,29 @@ app.secret_key = 'RebSearch'
 ############################################################################################################
 @app.route('/dropboxLogin')
 def dropboxLogin():
-    if not 'access_token' in session:
-        return redirect(url_for('dropbox_auth_start'))
-    else:
-        return redirect(url_for('home'))
+	if not 'access_token' in session:
+		return redirect(url_for('dropbox_auth_start'))
+	else:
+		return redirect(url_for('home'))
 
 @app.route('/dropbox-auth-start')
 def dropbox_auth_start():
-    return redirect(get_auth_flow().start())
+	return redirect(get_auth_flow().start())
 
 @app.route('/dropbox-auth-finish')
 def dropbox_auth_finish():
-    try:
-        access_token, user_id, url_state = get_auth_flow().finish(request.args)
-    except:
-        abort(400)
-    else:
-        session['access_token'] = access_token
-    return redirect(url_for('home'))
+	try:
+		access_token, user_id, url_state = get_auth_flow().finish(request.args)
+	except:
+		abort(400)
+	else:
+		session['access_token'] = access_token
+	return redirect(url_for('home'))
 
 def get_auth_flow():
-    redirect_uri = url_for('dropbox_auth_finish', _external=True)
-    return DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET, redirect_uri,
-                             session, 'dropbox-auth-csrf-token')
+	redirect_uri = url_for('dropbox_auth_finish', _external=True)
+	return DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET, redirect_uri,
+							 session, 'dropbox-auth-csrf-token')
 
 
 # General
@@ -91,27 +91,27 @@ def get_auth_flow():
 # home
 @app.route('/')
 def home():
-    if not 'access_token' in session:
-        return redirect(url_for('dropbox_auth_start'))
-    
-    else:
-        access_token = session['access_token']
-        client = dropbox.client.DropboxClient(access_token)
-        account_info = client.account_info()
+	if not 'access_token' in session:
+		return redirect(url_for('dropbox_auth_start'))
+	
+	else:
+		access_token = session['access_token']
+		client = dropbox.client.DropboxClient(access_token)
+		account_info = client.account_info()
 
-        print ZOTERO_USER_ID
+		print ZOTERO_USER_ID
 
-        return render_template('index.html',account_info=account_info,ZOTERO_USER_ID=ZOTERO_USER_ID)
+		return render_template('index.html',account_info=account_info,ZOTERO_USER_ID=ZOTERO_USER_ID)
 
 @app.route('/logout')
 def logout():
-    if not 'access_token' in session:
-        return "You ain't logged in!"
-    
-    else:
-        session.pop('access_token', None)
-        print session
-        return "Ya'll come back now, ya hear!"
+	if not 'access_token' in session:
+		return "You ain't logged in!"
+	
+	else:
+		session.pop('access_token', None)
+		print session
+		return "Ya'll come back now, ya hear!"
 
 
 # Zotero
@@ -119,30 +119,105 @@ def logout():
 
 @app.route('/zoteroTesting')
 def zoteroTesting():
-    goober = zot.items(limit=1,order="dateModified") 
-    last_modified = utilities.getLastZotSyncDate()   
+	goober = zot.items(limit=1,order="dateModified") 
+	last_modified = utilities.getLastZotSyncDate()   
 
-    return render_template('zoteroTesting.html',goober=goober,last_modified=last_modified)
+	return render_template('zoteroTesting.html',goober=goober,last_modified=last_modified)
 
 
 @app.route('/syncZotero')
 def syncZotero():
-    '''
-    1) Check last Zot-Sync date in Solr
-    2) Retrieve all items from Zotero modified after that date
-    3) For each
-        a) get metadata in useful form
-        b) create / update object in Fedora (PID = RebSearch_Account:ZoteroID)
-    4) Update last Zot-Synce date in Solr
-    '''
+	'''
+	1) Check last Zot-Sync date in Solr
+	2) Retrieve all items from Zotero modified after that date
+	3) For each
+		a) get metadata in useful form
+		b) create / update object in Fedora (PID = RebSearch_Account:ZoteroID)
+	4) Update last Zot-Synce date in Solr
+	'''
 
-    # get last sync date
-    LastZotSyncDate = utilities.getLastZotSyncDate()
+	# get last sync date
+	LastZotSyncDate = utilities.getLastZotSyncDate()    
+
+	return render_template('syncZotero.html',goober=goober)
 
 
-    
+@app.route('/fullIndexZotero')
+def fullIndexZotero():
+	'''
+	1) Retrieve items with zot.items(), limit = something, then using start as a cursor
+	* if limit 10, then bump start by 10 - KISS, keep it simple stupid.
+	'''
 
-    return render_template('syncZotero.html',goober=goober)
+	# settings    
+	start = 0
+	limit = 50
+	tcount = zot.num_items()
+
+	print "About to retrieve {tcount} items...".format(tcount=tcount)
+
+	# iterate through all Zotero citations
+	while start < tcount:	
+		print "Starting at item {start}".format(start=str(start))
+		zot_citation_chunk = zot.items(limit=limit,start=start,order="dateAdded")		
+		
+		# for each citation in chunk, do something
+		# MOVE TO CELERY TASK
+		for zot_citation in zot_citation_chunk:
+			try:
+				createFedZotObject(zot_citation)
+			except:
+				print "Could not create object."
+
+		# bump cursor pointer (start) by limit per result
+		start += limit
+
+	print "finis!"
+	return "All done."
+
+# function to create / update Fed object from Zot citation
+def createFedZotObject(zot_citation):
+	
+	'''
+	RebSearch:collection-* --> Collection at RebSearch level
+	RebSearch:CM-* --> Content Models at the RebSearch level
+	RebSearch:ZotCM-* --> Content Models at the Zotero level
+
+	Improvements
+	1) Derive simple DC from Zotero citation? Or do this later?
+	'''	
+	
+	# stick results in fedora object    
+	PID = "RebSearch:zotero-{group_id}-{zot_key}".format(group_id=zot_citation['group_id'],zot_key=zot_citation['key'])	
+
+	# create object
+	obj_create = eulfedora.models.DigitalObject(fedora_handle, pid=PID, create=True)
+	obj_create.label = zot_citation['title'].encode('utf-8').strip()	
+	obj_create.save()
+
+	# get handle
+	obj_handle = fedora_handle.get_object(PID)
+	
+	# set preliminary relationships	
+	obj_handle.add_relationship('info:fedora/fedora-system:def/relations-external#isMemberOfCollection','info:fedora/RebSearch:collection-ZoteroCitation')
+	obj_handle.add_relationship('info:fedora/fedora-system:def/relations-external#hasContentModel','info:fedora/RebSearch:CM-ZoteroCitation')
+	try:
+		obj_handle.add_relationship('info:fedora/fedora-system:def/relations-external#hasContentModel','info:fedora/RebSearch:ZotCM-{zot_type}'.format(zot_type=zot_citation['itemType']))
+	except:
+		"Citation type not found."
+	
+	# initialized DS object
+	newDS = eulfedora.models.DatastreamObject(obj_handle, "ZOTJSON", "Zotero JSON", control_group="M")    
+
+	# construct DS object	
+	newDS.mimetype = "application/json"
+	newDS.content = json.dumps(zot_citation)
+
+	print "Object {PID} created.".format(PID=PID)
+
+	# save constructed object
+	newDS.save()
+	
 
 
 # Dropbox
@@ -150,13 +225,20 @@ def syncZotero():
 @app.route('/dropboxTesting')
 def dropboxTesting():
 
-    dropboxHandle = dropboxHandles.instantiate(session['access_token'])
-    articles = dropboxHandle.metadata('/articles')
-    articles_count = len(articles['contents'])      
+	dropboxHandle = dropboxHandles.instantiate(session['access_token'])
+	articles = dropboxHandle.metadata('/articles')
+	articles_count = len(articles['contents'])      
 
-    return render_template('dropboxTesting.html',articles_count=articles_count)
+	return render_template('dropboxTesting.html',articles_count=articles_count)
 
 
+
+# Solr
+##############################################################################################################################
+'''
+1) First task, index Zotero Citations 
+2) Iterate through Dropbox items once files have been renamed, had full-text to Zotero Citation document
+'''
 
 ######################################################
 # Catch all - DON'T REMOVE
